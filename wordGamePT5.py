3709cec897d6effd4f7e861b5d242535163f1ac1
dpapa7 <dypapadopoulos@gmail.com>
2022-06-29 20:44:50 -0400

    parts4and5

---
+++ wordGamePT5.py
@@ -0,0 +1,76 @@
+from urllib.request import urlopen
+import random
+
+#create a list holding words from source
+word_list = urlopen("https://raw.githubusercontent.com/dwyl/english-words/master/words.txt").read().decode('utf-8')
+word_list = word_list.split('\n')
+
+#select a random word from list
+randomNUM = random.randint(0, len(word_list))
+word = word_list[randomNUM]
+#create list to add used words
+UsedWords = []
+
+for word in UsedWords:
+    if UsedWords(word) in word_list:
+        word_list.remove(UsedWords)
+    else:
+        break
+
+
+Guess = " "
+count = 7
+
+wins = 0
+loses = 0
+
+
+GameRestart = True
+
+while GameRestart:
+
+    Guess = input("\nenter your guess: ")
+    if count  == 0:
+        print(f"YOU LOSE!! \nThe correct word was {word}")
+        loses = loses + 1
+        print(f"Your performance so far\nWins:  {wins}\nLoses:  {loses}")
+        GameRestart = bool(input("\nThanks for playing, Would you like to play again?\nType something and press enter to restart the game, ONLY press enter to exit"))
+        count = 7
+        
+    
+    elif len(Guess) > 1:
+            
+            if Guess == word:
+                print("YOU WIN")
+                wins = wins +  1
+                print(f"Your performance so far\nWins:  {wins}\nLoses:  {loses}")
+            else:
+                print ("try again")
+                count = count - 1
+                print(f"Remaining Guesses: {count}")
+
+    else:
+            if Guess in word:
+                
+                for i in range(0,len(word)):
+                    
+                    if word[i] == Guess:
+                        print(word[i], end = "")
+                    
+                    elif word[i] == " ":
+                        print(" ", end = "")
+
+                    else:
+                        print("-", end = "")
+            
+            else:
+                print("Wrong Letter, Try again")
+                count = count - 1
+                print(f"Remaining Guesses: {count}")
+    
+    if GameRestart:
+        UsedWords.append(word)
+    else:
+        break    
+else:
+    print("Thanks for playing")
